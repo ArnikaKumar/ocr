0a8c69e73343e42eea50e5f54bbef42cc3886c36
unknown <arnikakumar@gmail.com>
2020-01-06 09:44:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import requests
+import json
+  
+app = Flask(__name__) 
+  
+@app.route('/') 
+def index(): 
+    return render_template('index.html')
+
+@app.route('/getDetails',methods=['POST']) 
+# This is not working for now
+def getDetails():
+    img = request.files['pic']
+    lang = request.form['lang']
+    payload = {'isOverlayRequired': False, 'apikey': 'b7c4ae2c5b88957', 'url':img, 'language': lang}
+    r = requests.post('https://api.ocr.space/parse/image', data=payload)
+    content = json.loads(r.content.decode())
+    print(content)
+    text = content['ParsedResults'][0]['ParsedText']
+    return text
+  
+if __name__ == '__main__': 
+    app.run(debug=True) 
